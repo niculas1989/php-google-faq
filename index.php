232c7b4dada4ad282f139c292cfe7f19701c6563
niculas1989 <nicolas.maranzano@libero.it>
2022-02-28 14:55:05 +0100

    added header and footer

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 //| Scrivere un array in cui poterci muovere per "rifocillare" la pagina
 
 $main_info = [
-    'section' => [
+    'oblio' => [
         'title' => 'Come state implementando la recente decisione della Corte di giustizia dell\'Unione europea (CGUE) relativa al diritto all\'oblio?',
         'paragraphs' => [
             'La recente decisione della Corte di giustizia dell\'Unione europea ha profonde conseguenze per i motori di ricerca in Europa. La Corte ha stabilito che alcuni utenti hanno il diritto di chiedere ai motori di ricerca come Google di rimuovere risultati relativi a chiavi di ricerca che includono il proprio nome. Per poter essere rimossi, i risultati visualizzati devono essere inadeguati, irrilevanti o non più rilevanti, o eccessivi.',
@@ -13,7 +13,52 @@ $main_info = [
             'Quando cerchi un nome, potresti trovare una notifica in cui viene spiegato che i risultati potrebbero essere stati modificati nel rispetto delle leggi europee per la protezione dei dati. Mostriamo questa notifica agli utenti europei quando cercano la maggior parte dei nomi, non soltanto le pagine che sono state soggette a una rimozione.'
         ]
     ],
-]
+    'privacy' => [
+        'title' => 'Come fa Google a proteggere la mia privacy e a tenere le mie informazioni al sicuro?',
+        'paragraphs' => [
+            'Sappiamo che la sicurezza e la privacy sono importanti per te e lo sono anche per noi. Per noi è imperativo offrire un\'elevata sicurezza e assicurarti che le tue informazioni sono al sicuro e accessibili quando ne hai bisogno.',
+            'Da quando questa decisione è stata pubblicata il 13 maggio 2014, abbiamo lavorato incessantemente per uniformarci a essa. Si tratta di una procedura complessa perché dobbiamo valutare ogni singola richiesta e effettuare un bilanciamento tra il diritto dell\'individuo a controllare i suoi dati personali ed il diritto di tutti di conoscere e distribuire le informazioni.',
+            'Siamo costantemente al lavoro per garantire un\'elevata sicurezza, proteggere la tua privacy e rendere Google ancora più efficace ed efficiente per te. Spendiamo centinaia di milioni di dollari ogni anno per la sicurezza e ci avvaliamo di esperti di fama mondiale in materia di sicurezza dei dati per mantenere le tue informazioni al sicuro. Abbiamo inoltre sviluppato strumenti per la sicurezza e la privacy di facile utilizzo come Google Dashboard, la verifica in due passaggi e Impostazioni annunci. Così, per quanto riguarda le informazioni che condividi con Google, hai il massimo controllo.',
+            'Puoi leggere ulteriori informazioni sulla sicurezza online, incluso come proteggere te e la tua famiglia online, nel Centro Google per la sicurezza online.',
+            'Scopri quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, lasciando a te il controllo.'
+        ]
+    ],
+    'account' => [
+        'title' => 'Perché il mio account è associato a un paese?',
+        'paragraphs' => [
+            'Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose:',
+            'La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti:
+            Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell\'Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.
+            Google LLC, con sede negli Stati Uniti, per il resto del mondo.
+            La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.',
+            'Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società consociata che li offre o dal paese a cui è associato il tuo account.',
+            'Stabilire il paese associato al tuo account',
+            'Quando crei un nuovo Account Google, lo associamo a un paese in base a dove è stato creato. Per quanto riguarda gli account creati almeno un anno fa, usiamo il paese da cui accedi solitamente ai servizi Google, in genere i servizi in cui hai trascorso più tempo nell\'ultimo anno.',
+            'I viaggi frequenti solitamente non influiscono sul paese associato al tuo account. Se ti trasferisci in un altro paese, potrebbe occorrere circa un anno per aggiornare l\'associazione del paese.',
+            'Se il paese associato al tuo account non corrisponde al tuo paese di residenza, il motivo potrebbe essere la differenza tra il paese in cui lavori e il paese in cui risiedi, l\'installazione di una rete privata virtuale (VPN) per mascherare il tuo indirizzo IP oppure la residenza vicino a un confine territoriale. Contattaci se ritieni che il paese associato al tuo account sia sbagliato.'
+        ]
+    ],
+    'ricerca' => [
+        'title' => 'Come faccio a rimuovere informazioni su di me dai risultati di ricerca di Google?',
+        'paragraphs' => [
+            'I risultati di ricerca di Google rispecchiano i contenuti pubblicamente disponibili sul Web. I motori di ricerca non possono rimuovere i contenuti direttamente dai siti web, quindi rimuovere risultati di ricerca da Google non consente di rimuovere i contenuti dal Web. Se desideri rimuovere qualcosa dal Web, devi contattare il webmaster del sito su cui sono pubblicati i contenuti e chiedergli di apportare una modifica. Inoltre, se, ai sensi delle leggi europee per la protezione dei dati, desideri richiedere la rimozione di determinate informazioni su di te visualizzate nei risultati di ricerca di Google, fai clic qui. Una volta che i contenuti saranno stati rimossi e che Google avrà rilevato l\'aggiornamento, le informazioni non verranno più visualizzate nei risultati di ricerca di Google. In caso di una richiesta di rimozione urgente, è inoltre possibile visitare la nostra pagina di assistenza per avere ulteriori informazioni.'
+        ]
+    ],
+    'click' => [
+        'title' => 'Quando faccio clic sui risultati della Ricerca Google, le mie chiavi di ricerca vengono inviate ai siti web?',
+        'paragraphs' => [
+            'In alcuni casi sì. Quando fai clic su un risultato della Ricerca Google, il tuo browser web potrebbe reindirizzare alla pagina web di destinazione anche l\'indirizzo Internet, o URL, della pagina dei risultati di ricerca sotto forma di URL referrer. Talvolta, l\'URL della pagina dei risultati di ricerca potrebbe contenere la query di ricerca che hai inserito. Se utilizzi la ricerca SSL (la funzione di ricerca criptata di Google), nella maggior parte dei casi i termini di ricerca non vengono inviati come parte dell\'URL negli URL referrer. Questo comportamento può fare eccezione, ad esempio se utilizzi alcuni browser meno diffusi. Ulteriori informazioni sulla ricerca SSL sono disponibili qui. Le query di ricerca o le informazioni contenute nell\'URL referrer potrebbero essere disponibili mediante Google Analytics o un\'API (Application Programming Interface). Inoltre, gli inserzionisti potrebbero ricevere informazioni relative all\' esatte parole chiave che hanno determinato il clic su un annuncio.'
+        ]
+    ],
+];
+
+$header_info = [
+    'Introduzione',
+    'Norme sulla privacy',
+    'Termini di servizio',
+    'Tecnologie',
+    'Domande frequenti'
+];
 
 ?>
 
@@ -36,11 +81,51 @@ $main_info = [
     footer {
         height: 100px;
     }
+
+    img {
+        height: 50px;
+    }
+
+    span {
+        font-size: 30px;
+    }
+
+    a {
+        text-decoration: none;
+        color: grey;
+    }
+
+    li {
+        list-style-type: none;
+        color: grey;
+    }
+
+    li:hover {
+        color: dodgerblue;
+        border-bottom: 2 px solid dodgerblue;
+        font-weight: 900;
+    }
 </style>
 
 <body>
     <!-- Parte superiore del body -->
-    <header></header>
+    <header>
+        <div id="header-top">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/Google_logo_%282010-2013%29.svg/800px-Google_logo_%282010-2013%29.svg.png" alt="Logo GOOGLE">
+            <span>
+                <a href="#">
+                    Privacy e Termini
+                </a>
+            </span>
+        </div>
+        <div id="header-bottom">
+            <ul class="d-flex">
+                <?php foreach ($header_info as $info) : ?>
+                    <li class="pe-5"><?= $info ?></li>
+                <?php endforeach ?>
+            </ul>
+        </div>
+    </header>
 
     <!-- Il fulcro delle info andranno qui -->
     <main>
@@ -55,7 +140,37 @@ $main_info = [
     </main>
 
     <!-- Parte finale del body -->
-    <footer></footer>
+    <footer>
+        <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">Google</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">Tutto su Google</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Privacy</a>
+                            </li>
+                        </ul>
+                        <form class="d-flex">
+                            <select id="colori" name="colori">
+                                <option value="Italiano">Italiano</option>
+                                <option value="Russo">Russo</option>
+                                <option value="Inglese">Inglese</option>
+                                <option value="Ucraino">Ucraino</option>
+                            </select>
+                        </form>
+                    </div>
+                </div>
+            </nav>
+
+        </div>
+    </footer>
 
 </body>
 
