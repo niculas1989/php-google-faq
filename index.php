00577b4366e1019366ac444e5e2485bc66fe35ba
niculas1989 <nicolas.maranzano@libero.it>
2022-02-28 13:44:47 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+scriveremo qualcosa qua
+*/
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Esercitazione - GOOGLE FAQ</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
